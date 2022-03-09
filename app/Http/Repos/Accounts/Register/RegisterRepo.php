26ab5b9eb3ff1d24daddf1575ccfdcdf0f554c97
MohammedBadry <mahamedbadry2003@gmail.com>
2022-03-09 14:42:24 +0200

    Update RegisterRepo.php

--- app/Http/Repos/Accounts/Register/RegisterRepo.php
+++ app/Http/Repos/Accounts/Register/RegisterRepo.php
@@ -17,21 +17,21 @@ class RegisterRepo implements RegisterInterface
     public function store($request)
     {
         //check the location in redis cache
-        $cachedlocation = Redis::get('location_'.$request->address.$request->city.$request->country);
+        $cachedlocation = Redis::get('location_'.$request->address1.$request->city.$request->country);
         if(isset($cachedlocation)) {
             //If exists get it from redis
             $location = json_decode($cachedlocation, true);
             $request['latitude'] = $location['lat'];
             $request['longitude'] = $location['lng'];
         } else {
-            $location = $this->location($request->address, $request->city, $request->country);
+            $location = $this->location($request->address1, $request->city, $request->country);
             $result = json_decode($location['body'], true);
             $cords = $result['results'][0]['geometry']['location'];
             if ($location['status'] == 200) {
                 $request['latitude'] = $cords['lat'];
                 $request['longitude'] = $cords['lng'];
                 //Save the location in redis cache
-                Redis::set('location_'.$request->address.$request->city.$request->country, json_encode(['lat' => $cords['lat'], 'lng' => $cords['lng']]));
+                Redis::set('location_'.$request->address1.$request->city.$request->country, json_encode(['lat' => $cords['lat'], 'lng' => $cords['lng']]));
             } else {
                 return response()->json([
                     'error' => true,
