ffb86dca8d7a02962743d1505610beb51f290ee1
MohammedBadry <mahamedbadry2003@gmail.com>
2022-03-10 11:48:42 +0200

    Update RegisterTest.php

--- tests/Feature/RegisterTest.php
+++ tests/Feature/RegisterTest.php
@@ -2,12 +2,57 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Client;
 use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class RegisterTest extends TestCase
 {
     use RefreshDatabase;
+
+    public function test_register_required_fields()
+    {
+        $res = $this->postJson('/api/v1/register');
+        $res->assertJsonValidationErrors('client_name');
+        $res->assertJsonValidationErrors('address1');
+        $res->assertJsonValidationErrors('city');
+        $res->assertJsonValidationErrors('state');
+        $res->assertJsonValidationErrors('country');
+        $res->assertJsonValidationErrors('phone_no1');
+        $res->assertJsonValidationErrors('first_name');
+        $res->assertJsonValidationErrors('last_name');
+        $res->assertJsonValidationErrors('email');
+        $res->assertJsonValidationErrors('password');
+        $res->assertJsonValidationErrors('phone');
+    }
+
+    public function test_register_unique_fields()
+    {
+        $user = User::factory()->create();
+
+        $new_user = $this->postJson('/api/v1/register', [
+            'client_name' => 'Ahmed Ali',
+            'address1' => 'Luxor',
+            'address2' => 'Tod',
+            'city' => 'Tod',
+            'state' => 'Luxor',
+            'country' => 'Egypt',
+            'phone_no1' => $user->client->phone_no1,
+            'phone_no2' => $user->client->phone_no2,
+            'zip' => '85842',
+            'first_name' => 'Test',
+            'last_name' => 'Test',
+            'email' => $user->email,
+            'password' => '123456',
+            'password_confirmation' => '123456',
+            'phone' => $user->phone,
+        ]);
+        $new_user->assertJsonValidationErrors('phone_no1');
+        $new_user->assertJsonValidationErrors('phone_no2');
+        $new_user->assertJsonValidationErrors('email');
+        $new_user->assertJsonValidationErrors('phone');
+    }
 
     public function test_client_can_register()
     {
@@ -30,7 +75,7 @@ class RegisterTest extends TestCase
             ]);
 
         $this->assertDatabaseHas('clients', [
-            'phone_no1' => '01000376267',
+            'phone_no1' => '01000123456',
         ]);
     }
 
